9a4b336238d0aea2027d270c443664f75ebde94e
Gabriele <gabriele250901@outlook.it>
2023-06-05 10:35:33 +0200

    done

--- src/components/exercise21/exercise21.jsx
+++ src/components/exercise21/exercise21.jsx
@@ -7,22 +7,39 @@ export class UncontrolledLogin extends React.Component {
     event.preventDefault();
 
     const { onLogin } = this.props;
-    const username = this.usernameRef.current.value
-    const password = this.passwordRef.current.value
-    
-    onLogin({ username, password});
+    const username = this.usernameRef.current.value;
+    const password = this.passwordRef.current.value;
+
+    onLogin({ username, password });
   };
 
+  tryingDisabled = () => {
+    const btnD = document.querySelector("#provaD");
+    if (this.usernameRef.current.value && this.passwordRef.current.value) {
+      btnD.disabled = false;
+    } else {
+      btnD.disabled = true;
+    }
+  };
   render() {
-    const isDisabled = !this.usernameRef.current  || !this.passwordRef.current ;
     return (
       <div>
         <form onSubmit={this.handleLogin}>
           <h1>Uncontrolled Login</h1>
-          <input type="text" name="username" ref={this.usernameRef}  />
-          <input type="password" name="password" ref={this.passwordRef}  />
+          <input
+            type="text"
+            name="username"
+            onChange={this.tryingDisabled}
+            ref={this.usernameRef}
+          />
+          <input
+            type="password"
+            name="password"
+            onChange={this.tryingDisabled}
+            ref={this.passwordRef}
+          />
 
-          <button type="submit" disabled={isDisabled}>
+          <button type="submit" id="provaD">
             Login
           </button>
           <button type="reset">Reset</button>
@@ -31,4 +48,3 @@ export class UncontrolledLogin extends React.Component {
     );
   }
 }
-
