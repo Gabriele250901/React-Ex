24b90ae527bed031c1b2d112b14a92d6e7bccbcf
Gabriele <gabriele250901@outlook.it>
2023-06-05 17:02:34 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-import React from 'react';
-import './App.css';
-import { Hello } from './components/exercise1/hello';
-import { Message } from './components/exercise1/message';
-import {  Welcome } from './components/exercise2/exercise2';
-import { Age } from './components/exercise6/exercise6';
-import { Counter } from './components/exercise12/exercise12';
-import { CounterDisplay } from './components/exercise12/exercise14';
-import { ClickCounter } from './components/exercise12/exercise16';
-import { ClickTracker } from './components/exercise17/exercise17';
-import { InteractiveWelcome } from './components/exercise18/exercise18';
-import { Login } from './components/exercise19/exercise19';
+import React from "react";
+import "./App.css";
+import { Hello } from "./components/exercise1/hello";
+import { Message } from "./components/exercise1/message";
+import { Welcome } from "./components/exercise2/exercise2";
+import { Age } from "./components/exercise6/exercise6";
+import { Counter } from "./components/exercise12/exercise12";
+import { CounterDisplay } from "./components/exercise12/exercise14";
+import { ClickCounter } from "./components/exercise12/exercise16";
+import { ClickTracker } from "./components/exercise17/exercise17";
+import { InteractiveWelcome } from "./components/exercise18/exercise18";
+import { Login } from "./components/exercise19/exercise19";
 
 
-function App() {
-  return (
-    <div>
-      <Hello/>
-      <Message/>
 
-     <Login/>
-     
-      <ClickTracker/>
-      
-    </div>
-  );
+
+class App extends React.Component {
+  onLogin = (data1) => {
+    console.log(data1)
+  }
+  render() {
+    return (
+      <div>
+        <Hello />
+        <Message />
+
+        <Login onclick={this.onLogin}/>
+
+        <ClickTracker />
+      </div>
+    );
+  }
 }
 
 export default App;
 
-// ogni volta che mi correggerai un esercizio togliero` la classe dall'app in modo da non fare casini 
+// ogni volta che mi correggerai un esercizio togliero` la classe dall'app in modo da non fare casini
