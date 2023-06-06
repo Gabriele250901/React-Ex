cecb38bc6a9dcc2bef93897f3f9731bcd28795bc
Gabriele <gabriele250901@outlook.it>
2023-06-06 10:32:39 +0200

    done

--- src/App.js
+++ src/App.js
@@ -12,37 +12,38 @@ import { InteractiveWelcome } from "./components/exercise18/exercise18";
 import { Login } from "./components/exercise19/exercise19";
 import { UncontrolledLogin } from "./components/exercise21/exercise21";
 import { TodoList } from "./components/exercise23/exercise23";
+import { LanguageContext } from "./components/exercise28/exercise28";
+import { DisplayLanguage } from "./components/exercise28/exercise28pt2";
 
-function App() {
-  return (
-    <div>
-      <Hello />
-      <Message />
-
-      <TodoList 
-        render={(items, deleteTodo) => {
-          
-          return (
-            <div>
-              {items.map((todo) => (
-                <li key={todo.id} >
-                  {todo.name}
-                  <button onClick={() => deleteTodo(todo.id)}>
-                    &times;
-                  </button>
-                </li>
-              ))}
-              
-            </div>
-          );
-        }}
-      />
-
-      <ClickTracker />
-    </div>
-  );
+ class App extends React.Component {
+  state={
+    language: "en"
+  }
+  changingLanguage = (event)=> {
+    this.setState({
+      language: event.target.value
+    })
+    
+  }
+  componentDidUpdate(){
+    console.log(this.state)
+  }
+  render() {
+    return (
+      <div>
+        <select value={this.state.language} onChange={this.changingLanguage}>
+          <option value="en">English</option>
+          <option value="It">Italiano</option>
+        </select>
+        <LanguageContext.Provider value={this.state.language}>
+          <DisplayLanguage/>
+        </LanguageContext.Provider>
+        <Hello />
+        <Message />
+      </div>
+    );
+  }
 }
 
-export default App;
-
+export default App
 // ogni volta che mi correggerai un esercizio togliero` la classe dall'app in modo da non fare casini
---
+++ src/components/exercise28/exercise28pt2.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export class DisplayLanguage extends React.Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
