774feae475532464dde7c2aa30bcfd3bb215813b
Gabriele <gabriele250901@outlook.it>
2023-06-20 18:34:07 +0200

    done

--- src/components/exercise40/exercise40.jsx
+++ src/components/exercise40/exercise40.jsx
@@ -6,15 +6,12 @@ export function CarDetails({
   const modelRef = useRef();
   const yearRef = useRef();
   const colorRef = useRef();
+
   useEffect(() => {
     modelRef.current.value = initialValue.model;
     yearRef.current.value = initialValue.year;
     colorRef.current.value = initialValue.color;
-  }, []);
-
-  useEffect(()=> {
-    
-  },[initialValue])
+  }, [initialValue]);
   function handleFormChange(event) {
     event.preventDefault();
   }
@@ -22,30 +19,12 @@ export function CarDetails({
   return (
     <div>
       <form onSubmit={handleFormChange}>
-        <input
-          type="text"
-          name="model"
-          ref={modelRef}
-         
-        />
-        <input
-          type="text"
-          name="year"
-          ref={yearRef}
-         
-        />
-        <input
-          type="text"
-          name="color"
-          ref={colorRef}
-          
-        />
+        <input type="text" name="model" ref={modelRef} />
+        <input type="text" name="year" ref={yearRef} />
+        <input type="text" name="color" ref={colorRef} />
       </form>
     </div>
   );
 }
 
 
-/* ciao jeremy l'esercizio manca di una parte , non ho idea di come si possa fare, ho provato con una funzione onChange,
-con un sideEffect ogni volta che il valore cambia ma nulla non riesco a far resettare ogni volta che il valore 
-cambia , in caso se vuoi lo possiamo vedere in live o non saprei */
